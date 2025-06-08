1b1f913d5e7544ee492fed826f9c01b97c2f52e1
3numco <1good_evening@mail.ru>
2025-06-08 05:58:03 +0300

    finalize

---
+++ src/metric_impl/parameters_count.cpp
@@ -0,0 +1,21 @@
+#include <array>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <filesystem>
+#include <fstream>
+#include <functional>
+#include <iostream>
+#include <ranges>
+#include <sstream>
+#include <string>
+#include <variant>
+#include <vector>
+
+#include "metric_impl/parameters_count.hpp"
+
+namespace analyser::metric::metric_impl {
+
+// здесь ваш код
+
+}  // namespace analyser::metric::metric_impl
